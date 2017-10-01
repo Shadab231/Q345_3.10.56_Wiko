95e939ef419da31ac72a26303197717a0f069ae0
shadab231 <husainshadab85@gmail.com>
2017-10-01 09:18:03 +0530

    v3.10.56

---
+++ kernel/power/.tuxonice_bio.o.cmd
@@ -0,0 +1,1 @@
+cmd_kernel/power/tuxonice_bio.o := /home/shadab/NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86/bin/arm-linux-androideabi-ld -EL    -r -o kernel/power/tuxonice_bio.o kernel/power/tuxonice_bio_core.o kernel/power/tuxonice_bio_chains.o kernel/power/tuxonice_bio_signature.o 
